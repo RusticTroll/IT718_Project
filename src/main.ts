df1aac134b212936c4ef8c0fedad9b328d963937
RusticTroll <willzerboy@gmail.com>
2025-03-24 11:25:14 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import AmplifyVue from '@aws-amplify/ui-vue'
+
+import App from './App.vue'
+import router from './router'
+
+import awsconfig from '../amplify_outputs.json'
+import { Amplify } from 'aws-amplify';
+Amplify.configure(awsconfig)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(AmplifyVue)
+app.use(router)
+
+app.mount('#app')

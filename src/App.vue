02b032de23905c70f8094e96cb505851f4da88a3
RusticTroll <willzerboy@gmail.com>
2025-04-07 16:46:31 -0400

    Made the basic UI responsive & added home button

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,33 @@
 import '@aws-amplify/ui-vue/styles.css'
 
 import { RouterLink, RouterView } from 'vue-router'
+import MagnifyingGlass from './components/SVGs/MagnifyingGlass.vue'
+import HomeImage from './components/SVGs/HomeImage.vue'
 </script>
 
 <template>
-  <nav class="w-32 min-w-32 flex-1 flex flex-col justify-items-stretch text-end">
-    <RouterLink to="/" class="content-center h-12">
-      <span class="align-middle pr-2">Home</span>
+  <nav
+    class="md:w-32 md:min-w-32 flex-1 flex flex-col justify-items-stretch md:text-end not-md:flex-row not-md:text-center not-md:w-auto not-md:flex-none text-lg"
+  >
+    <RouterLink
+      to="/"
+      class="content-center h-12 not-md:flex-1 flex items-center not-md:justify-center"
+    >
+      <HomeImage :size="48" stroke="hsla(160, 100%, 37%, 1)" />
+      <span class="align-middle pl-2">Home</span>
     </RouterLink>
-    <RouterLink to="/search" class="content-center h-12 relative">
-      <span class="align-middle pr-2">Search</span>
+    <RouterLink
+      to="/search"
+      class="content-center h-12 not-md:flex-1 flex items-center not-md:justify-center"
+    >
+      <MagnifyingGlass :size="48" stroke="hsla(160, 100%, 37%, 1)" />
+      <span class="align-middle pl-2">Search</span>
     </RouterLink>
   </nav>
 
-  <RouterView class="flex-none w-144 border-1 border-gray-500"/>
+  <RouterView class="flex-none md:w-144 not-md:w-full not-md:flex-grow border-1 border-gray-500" />
 
-  <div class="w-16 min-w-0 flex-1"/>
+  <div class="w-16 flex-1 not-md:max-h-0" />
 </template>
 
 <style scoped>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import MagnifyingGlass from '@/components/SVGs/MagnifyingGlass.vue';
+import MagnifyingGlass from '@/components/SVGs/MagnifyingGlass.vue'
 </script>
 
 <template>
-  <main class="">
-    <div class="flex flex-row justify-center pt-4 pb-4 relative border-b-1 border-gray-500">
-      <input class="border-1 border-gray-500 rounded-full h-13 w-116 pl-4 pr-12 focus:outline-none focus:border-green-500"/>
-      <MagnifyingGlass :size="36" class="absolute right-8 top-6 bg-gray-500 hover:bg-gray-400 rounded-full"/>
+  <main class="flex flex-col">
+    <div class="flex flex-row justify-center pt-4 pb-4 border-b-1 border-gray-500">
+      <div class="border-1 border-gray-500 rounded-full h-12 w-116 focus:border-green-500 relative">
+        <input class="pl-4 pr-12 focus:outline-none" />
+        <MagnifyingGlass
+          :size="36"
+          class="absolute right-1.25 top-1.25 bg-gray-500 hover:bg-gray-400 rounded-full"
+        />
+      </div>
     </div>
+    <div></div>
   </main>
-
 </template>
